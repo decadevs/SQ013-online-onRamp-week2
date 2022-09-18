b737d97474deffc68a501424671dc6fa0a70458c
EsChris <themainchris@gmail.com>
2022-09-18 22:12:09 +0200

    newly committed

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,16 @@
 function squareDigit(str) {
+    let returnStr = '';
+    for (let i = 0; i < str.length; i++) {
+     let num = Number(str[i])
+     returnStr += num * num;
+    }
+     console.log(returnStr)
+  }
+  squareDigit('9119')
+  squareDigit('289') 
+  //46481
+  squareDigit('345') 
+  //91625
+  squareDigit('7772')
  
- }
-  module.exports =squareDigit
+  // module.exports =squareDigit

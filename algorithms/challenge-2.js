f5bcbe37a15081ea816ddb2a1f833f8cb1e93e5f
Algebra0001 <iyusuf023112@gmail.com>
2022-09-19 01:19:00 +0100

    New commit

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,18 +1,18 @@
-function sumOfTwoSmallestNums(arr){
-    let result = []
-    for(let i=0; i<arr.length; i++){
-      if(arr[i]<result[0]||result[0]==null){
-        if(result[0]<result[1] ||result[1]==null){
-          result[0]=result[1]
-        } 
-          result[0]=arr[i]
-        } else if(arr[i]<result[1]||result[1]==null){
-          result[1]=arr[i]
+function sumOfTwoSmallestNums(arr) {
+    var longestArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] < longestArr[0] || longestArr[0] == null) {
+        if (longestArr[0] < longestArr[1] || longestArr[1] == null) {
+          longestArr[1] = longestArr[0];
+        }
+        longestArr[0] = arr[i];
+      } else if (arr[i] < longestArr[1] || longestArr[1] == null) {
+        longestArr[1] = arr[i];
       }
     }
-    return result[0]+result[1]
+    return longestArr[0] + longestArr[1];
   }
-  console.log(sumOfTwoSmallestNums([19, 5, 42, 2, 77]))
-  console.log(sumOfTwoSmallestNums([80, 33, 44,55,1,2,14]))
-
+  console.log(sumOfTwoSmallestNums([19, 5, 42, 2, 77]));
+console.log(sumOfTwoSmallestNums([10, 343445353, 3453445, 3453545353453]));
+console.log(sumOfTwoSmallestNums([80, 33, 44,55,1,2,14]))
 module.exports = sumOfTwoSmallestNums
--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,14 +1,18 @@
-const letters = "abcdefghijklmnopqrstuvwxyz";
 function alphabetPosition(text) {
-    let result = [];
-    for (let i = 0; i < text.length; i++) {
-      let letter = text[i].toLowerCase();
-      if (letters.includes(letter)) {
-        result.push(letters.indexOf(letter) + 1);
-      }
+  if (text.match(/[a-z]/gi)) {
+    let justAlphabet = text.match(/[a-z]/gi).join("").toLowerCase();
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let a = [];
+    for (let i = 0; i < justAlphabet.length; i++) {
+      a.push(alphabet.indexOf(justAlphabet[i]) + 1);
     }
-    return result.join(" ");
- }
+    return a.join(" ");
+  } else {
+    return "";                                               
+  }
+}
+console.log(alphabetPosition("decadev"))
+console.log(alphabetPosition('success'))
+console.log(alphabetPosition('developer'))
 
   module.exports = alphabetPosition
-  console.log(alphabetPosition('sucess'));
